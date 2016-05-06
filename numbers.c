77db0fcdd5f9eab99569785641738bdf380cc963
Rafiul Hye <rafiul.hye@bjitgroup.com>
2016-05-06 14:50:05 +0600

    changed in the C file

--- numbers.c
+++ numbers.c
@@ -3,10 +3,18 @@
 int main(){
 	int i,n;
 	n=100;
+	int p;
 	
 	for(i=0;i<=n;i++){
-		printf("%d\n",i);	
+		printf("%d\n",i);
 	}
-
+	
+	int j,m;
+	m=10;
+for(j=0;j<=m;j++)
+	{
+		printf("%d\n",(long)j);
+	}
+	
 	return 0;
 }
